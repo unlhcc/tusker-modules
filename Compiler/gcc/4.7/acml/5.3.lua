048561f73681941e20ffee452768cd74ff028efb
Adam Caprez <acaprez2@unl.edu>
2014-07-30 17:31:45 -0500

    Added module files for ACML.

---
+++ Compiler/gcc/4.7/acml/5.3.lua
@@ -0,0 +1,21 @@
+local help_message = [[
+ACML 5.3
+
+Loads the AMD Core Math Library (ACML).
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: ACML")
+whatis("Version: 5.3")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://developer.amd.com/tools-and-sdks/cpu-development/amd-core-math-library-acml")
+
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/acml/5.3/gfortran64_fma4/lib")
+setenv("CFLAGS",     "-I/util/opt/acml/5.3/gfortran64_fma4/include")
+setenv("FFLAGS",     "-I/util/opt/acml/5.3/gfortran64_fma4/include")
+setenv("CXXFLAGS",     "-I/util/opt/acml/5.3/gfortran64_fma4/include")
+setenv("F90FLAGS",     "-I/util/opt/acml/5.3/gfortran64_fma4/include")
+setenv("FCFLAGS",     "-I/util/opt/acml/5.3/gfortran64_fma4/include")
