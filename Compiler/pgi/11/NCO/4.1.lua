bdcc810a14ffd980e06daa917faa2a68aadfc26f
acaprez2 <acaprez2@unl.edu>
2014-07-30 16:37:13 -0500

    Module file for NCO 4.1.

---
+++ Compiler/pgi/11/NCO/4.1.lua
@@ -0,0 +1,22 @@
+local help_message = [[
+NCO 4.1
+
+This module loads the NetCDF Operators package, built with
+the PGI compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: NCO")
+whatis("Version: 4.1")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://nco.sourceforge.net")
+
+prepend_path("PATH",		    "/util/opt/NCO/4.1/pgi/11/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/NCO/4.1/pgi/11/lib")
+prepend_path("MANPATH",             "/util/opt/NCO/4.1/pgi/11/share/man")
+prepend_path("INCLUDE",             "/util/opt/NCO/4.1/pgi/11/include")
+
+family("NCO")
