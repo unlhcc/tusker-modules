0d2751cce3bd197d9c9b37cb2bcbcae1b7059f44
acaprez2 <acaprez2@unl.edu>
2014-07-30 15:03:31 -0500

    Compiler module files.

---
+++ Core/compiler/intel/11.lua
@@ -0,0 +1,40 @@
+local help_message = [[
+Intel Compilers 11 (2011.9)
+
+This module loads Intel Compiler variables.
+The command directory is added to PATH.
+The library directory is added to LD_LIBRARY_PATH.
+The include directory is added to INCLUDE.
+The man     directory is added to MANPATH.
+]]
+
+help(help_message,"\n")
+
+whatis("Name: Intel Compilers")
+whatis("Version: 11")
+whatis("Category: compiler")
+whatis("Keywords: System, compiler")
+whatis("URL: http://www.intel.com")
+
+prepend_path("PATH",                "/util/comp/intel/11/bin/intel64")
+prepend_path("LD_LIBRARY_PATH",     "/util/comp/intel/11/lib/intel64")
+prepend_path("LIBRARY_PATH",     "/util/comp/intel/11/lib/intel64")
+prepend_path("MANPATH",             "/util/comp/intel/11/man/en_US")
+prepend_path("INCLUDE",             "/util/comp/intel/11/include")
+prepend_path("MODULEPATH",          "/util/opt/modulefiles/Compiler/intel/11")
+
+setenv("CC",	"icc")
+setenv("FC",	"ifort")
+setenv("F90",	"ifort")
+setenv("F77",	"ifort")
+setenv("CXX",	"icpc")
+setenv("INTEL_LICENSE_FILE", 	"/util/comp/intel/11/licenses/USE_SERVER.lic")
+
+setenv("CFLAGS",	"-msse3")
+setenv("FFLAGS",	"-msse3")
+setenv("CXXFLAGS",        "-msse3")
+setenv("F90FLAGS",        "-msse3")
+setenv("FCFLAGS",        "-msse3")
+
+
+family("compiler")
---
+++ Core/compiler/pgi/14.lua
@@ -0,0 +1,38 @@
+local help_message = [[
+PGI Compilers 14
+
+This module loads PGI Compiler variables.
+The command directory is added to PATH.
+The library directory is added to LD_LIBRARY_PATH.
+The include directory is added to INCLUDE.
+The man     directory is added to MANPATH.
+]]
+
+help(help_message,"\n")
+
+whatis("Name: PGI Compilers")
+whatis("Version: 14")
+whatis("Category: compiler")
+whatis("Keywords: System, compiler")
+whatis("URL: http://www.pgroup.com")
+
+prepend_path("PATH",                "/util/comp/pgi/linux86-64/14/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/comp/pgi/linux86-64/14/lib")
+prepend_path("LD_LIBRARY_PATH",     "/util/comp/pgi/linux86-64/14/libso")
+prepend_path("MANPATH",             "/util/comp/pgi/linux86-64/14/man")
+prepend_path("INCLUDE",             "/util/comp/pgi/linux86-64/14/include")
+prepend_path("MODULEPATH",          "/util/opt/modulefiles/Compiler/pgi/14")
+setenv("CC",	"pgcc")
+setenv("FC",	"pgf90")
+setenv("F90",	"pgf90")
+setenv("F77",	"pgf77")
+setenv("CPP",	"pgcc -E")
+setenv("CXX",	"pgcpp")
+
+setenv("CFLAGS",        "-tp=bulldozer")
+setenv("FFLAGS",        "-tp=bulldozer")
+setenv("CXXFLAGS",        "-tp=bulldozer")
+setenv("F90FLAGS",        "-tp=bulldozer")
+setenv("FCFLAGS",        "-tp=bulldozer")
+
+family("compiler")
