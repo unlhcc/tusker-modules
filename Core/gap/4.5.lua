0f9219c440d312e23f92abef1dc5abbd811d456b
Adam Caprez <acaprez2@unl.edu>
2014-07-31 15:27:21 -0500

    Added module for GAP 4.5.

---
+++ Core/gap/4.5.lua
@@ -0,0 +1,17 @@
+local help_message = [[
+GAP - Groups, Algorithms, Programming -
+a System for Computational Discrete Algebra
+
+Version 4.5
+]]
+
+help(help_message,"\n")
+
+whatis("Name: GAP")
+whatis("Version: 4.5")
+whatis("Category: runtime, library")
+whatis("Keywords: Runtime, Library")
+whatis("Description: GAP is a system for computational discrete algebra, with particular emphasis on Computational Group Theory.")
+whatis("URL: http://www.gap-system.org")
+
+prepend_path("PATH","/util/opt/gap/gap4r5/gcc/4.6/bin")
