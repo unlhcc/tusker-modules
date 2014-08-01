ab8d4e04d0d0a4e575914be9baa3d8d883c7b456
Adam Caprez <acaprez2@unl.edu>
2014-08-01 15:16:43 -0500

    Added module file for Oases 0.2.8.

---
+++ Core/oases/0.2.8.lua
@@ -0,0 +1,15 @@
+help (
+[[
+This module loads Oases built with GCC.
+
+Version 0.2.8.
+]])
+
+whatis("Name: Oases")
+whatis("Version: 0.2.8")
+whatis("Category: computational biology, genomics")
+whatis("Keywords:  Biology, Genomics, Assembly, Sequencing")
+whatis("Description: Oases - De novo transcriptome assembler for very short reads.")
+whatis("URL: http://www.ebi.ac.uk/~zerbino/oases")
+
+prepend_path("PATH"       ,"/util/opt/oases/0.2.8/gcc/4.4/bin")
