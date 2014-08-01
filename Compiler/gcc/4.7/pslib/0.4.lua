cf25049d1a83d9b60edcc5b0f4717e0639bbe53e
Adam Caprez <acaprez2@unl.edu>
2014-08-01 15:29:00 -0500

    Added module file for pslib 0.4.

---
+++ Compiler/gcc/4.7/pslib/0.4.lua
@@ -0,0 +1,16 @@
+local help_message = [[
+
+Loads the pslib package. 
+
+Version 0.4.
+]]
+
+help(help_message,"\n")
+
+whatis("Name: pslib")
+whatis("Version: 0.4")
+whatis("Category: library, graphics")
+whatis("Keywords: System, Library")
+whatis("URL: http://pslib.sourceforge.net")
+
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/pslib/0.4/gcc/4.7/lib")
