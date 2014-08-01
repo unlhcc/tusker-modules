2cf32bd7127a96d540e72d355063cdb1e4749c70
Adam Caprez <acaprez2@unl.edu>
2014-08-01 11:45:44 -0500

    Added module files for HDF-EOS 5 1.14.

---
+++ Compiler/intel/12/hdfeos5/1.14.lua
@@ -0,0 +1,20 @@
+local help_message = [[
+hdfeos5 1.14
+
+This module loads HDF-EOS 5, built with the Intel
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: hdfeos5")
+whatis("Version: 1.14")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://hdfeos.org")
+
+prepend_path("LIBDIR",                "/util/opt/hdfeos5/1.14/intel/12/lib")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/hdfeos5/1.14/intel/12/lib")
+
+family("hdfeos")
