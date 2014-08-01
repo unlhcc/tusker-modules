ffaddb0399414743addc78523fd67e7fb9a63629
Adam Caprez <acaprez2@unl.edu>
2014-08-01 16:42:15 -0500

    Added module file for Scythe 0.991.

---
+++ Core/scythe/0.991.lua
@@ -0,0 +1,15 @@
+help (
+[[
+This module loads scythe built with GCC.
+
+Version 0.991
+]])
+
+whatis("Name: scythe")
+whatis("Version: 0.991")
+whatis("Category: computational biology, genomics")
+whatis("Keywords:  Biology, Genomics, Sequencing")
+whatis("Description: Scythe - A Bayesian adapter trimmer.")
+whatis("URL: https://github.com/vsbuffalo/scythe")
+
+prepend_path("PATH"       ,"/util/opt/scythe/0.991/gcc/4.4/bin")
