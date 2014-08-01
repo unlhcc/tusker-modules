5ac7736fdcb64f448901f1d525a41d0ef48cc9a1
Adam Caprez <acaprez2@unl.edu>
2014-08-01 11:39:01 -0500

    Added module files for HDF-EOS 2 2.18.

---
+++ Compiler/gcc/4.7/hdfeos2/2.18.lua
@@ -0,0 +1,20 @@
+local help_message = [[
+hdfeos 2.18
+
+This module loads HDF-EOS, built with the GCC 
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: hdfeos")
+whatis("Version: 2.18")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://hdfeos.org")
+
+prepend_path("LIBDIR",                "/util/opt/hdfeos/2.18/gcc/4.7/lib")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/hdfeos/2.18/gcc/4.7/lib")
+
+family("hdfeos")
