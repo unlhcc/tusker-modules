9996bb5e8ae1a65454be1f131d4411cb1602b01b
Adam Caprez <acaprez2@unl.edu>
2014-08-01 15:30:27 -0500

    Added description to module file.

--- Compiler/gcc/4.7/pslib/0.4.lua
+++ Compiler/gcc/4.7/pslib/0.4.lua
@@ -12,5 +12,6 @@ whatis("Version: 0.4")
 whatis("Category: library, graphics")
 whatis("Keywords: System, Library")
 whatis("URL: http://pslib.sourceforge.net")
+whatis("Description: pslib is a C-library to create PostScript files on the fly.")
 
 prepend_path("LD_LIBRARY_PATH",     "/util/opt/pslib/0.4/gcc/4.7/lib")
