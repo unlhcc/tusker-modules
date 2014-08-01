937dd5725bd250a36fa3163dad7e3f4ce3e9f477
Adam Caprez <acaprez2@unl.edu>
2014-08-01 16:55:18 -0500

    Added module file for SGA 0.10.

---
+++ Core/sga/0.10.lua
@@ -0,0 +1,15 @@
+help (
+[[
+This module loads sga built with GCC.
+
+Version 0.10.
+]])
+
+whatis("Name: sga")
+whatis("Version: 0.10")
+whatis("Category: computational biology, genomics")
+whatis("Keywords:  Biology, Genomics, Assembly")
+whatis("Description: SGA is a de novo assembler designed to assemble large genomes from high coverage short read data.")
+whatis("URL: https://github.com/jts/sga/wiki")
+
+prepend_path("PATH"       ,"/util/opt/sga/0.10/gcc/4.4/bin")
