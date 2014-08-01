1c34afd0450626b6a984fb75a4574d0a6faf2534
Adam Caprez <acaprez2@unl.edu>
2014-08-01 15:22:52 -0500

    Added module file for PhymmBL 4.0.

---
+++ Core/phymmbl/4.0.lua
@@ -0,0 +1,15 @@
+help (
+[[
+This module loads phymmbl built with GCC.
+
+Version 4.0.
+]])
+
+whatis("Name: phymmbl")
+whatis("Version: 4.0")
+whatis("Category: computational biology, genomics")
+whatis("Keywords:  Biology, Genomics, Taxonomy")
+whatis("Description: PhymmBL - Taxonomic Classification of Metagenomic Short Reads.")
+whatis("URL: http://www.cbcb.umd.edu/software/phymm")
+
+prepend_path("PATH"       ,"/util/opt/phymmbl/4.0/gcc/4.4")
