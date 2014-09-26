962e4b0d62e49b34d7542d76547ab38a761f681b
Adam Caprez <acaprez2@unl.edu>
2014-09-26 10:52:27 -0500

    Add module file for MrBayes 3.2.

--- Compiler/gcc/4.8/mrbayes/3.2.lua
+++ Compiler/gcc/4.8/mrbayes/3.2.lua
@@ -2,17 +2,17 @@ help(
 [[
 MrBayes is a program for Bayesian inference and model choice across a wide range of phylogenetic and evolutionary models
 
-Version 3.2.2
+Version 3.2
 ]]
 )
 
 whatis("Name: MrBayes")
-whatis("Version: 3.2.2")
+whatis("Version: 3.2")
 whatis("Category: computational biology, phylogeny")
 whatis("Keywords: Biology, Genomics, phylogeny")
 whatis("URL: http://mrbayes.sourceforge.net/")
 whatis("Description: MrBayes - a generic tool for phylogenetic analysis.")
 
 
-prepend_path("PATH",              "/util/opt/BCRF/mrbayes/3.2.2/gcc/4.8/bin")
-prepend_path("MANPATH",		  "/util/opt/BCRF/mrbayes/3.2.2/documentation/commref_mb3.2.2.txt")
+prepend_path("PATH",              "/util/opt/BCRF/mrbayes/3.2/gcc/4.8/bin")
+prepend_path("MANPATH",		  "/util/opt/BCRF/mrbayes/3.2/documentation/commref_mb3.2.2.txt")
