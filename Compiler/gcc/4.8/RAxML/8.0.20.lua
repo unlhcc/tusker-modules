3d118a1ea85682342812a00e7a19909d96fd741a
Seongil Eyun <eyun@login.tusker.hcc.unl.edu>
2014-09-26 12:49:47 -0500

    RAxML 8.0.20

---
+++ Compiler/gcc/4.8/RAxML/8.0.20.lua
@@ -0,0 +1,18 @@
+help(
+[[
+RAxML is a program for sequential and parallel Maximum Likelihood based inference of large phylogenetic trees
+
+Version 8.0.20
+]]
+)
+
+whatis("Name: RAxML")
+whatis("Version: 8.0.20")
+whatis("Category: computational biology, phylogeny")
+whatis("Keywords: Biology, Phylogeny")
+whatis("URL: http://sco.h-its.org/exelixis/web/software/raxml/")
+whatis("Description: RAxML - a generic tool for phylogenetic analysis.")
+
+
+prepend_path("PATH",              "/util/opt/BCRF/RAxML/8.2.20/")
+prepend_path("MANPATH",		  "/util/opt/BCRF/RAxML/8.2.20/")
