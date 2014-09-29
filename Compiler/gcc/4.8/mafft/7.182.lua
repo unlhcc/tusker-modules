1cb1b38787f4df7a163f4df89c4875a39f9cdb42
Seongil Eyun <eyun@login.tusker.hcc.unl.edu>
2014-09-29 11:26:24 -0500

    MAFFT

---
+++ Compiler/gcc/4.8/mafft/7.182.lua
@@ -0,0 +1,17 @@
+help(
+[[
+MAFFT is a program for multiple alignment program for amino acid or nucleotide sequences.
+
+Version 7.182
+]]
+)
+
+whatis("Name: MAFFT")
+whatis("Version: 7.182")
+whatis("Category: computational biology, alignments")
+whatis("Keywords: Biology, Genomics, alignments")
+whatis("URL: http://mafft.cbrc.jp")
+whatis("Description: MAFFT - a generic tool for sequence alingments.")
+
+prepend_path("PATH",              "/util/opt/BCRF/mafft/7.182/gcc/4.8/bin")
+prepend_path("MANPATH",		  "/util/opt/BCRF/mafft/7.182/share/man/man1")
