f7e9f038676be7ec064232bb92405fff4ae487db
Adam Caprez <acaprez2@unl.edu>
2014-09-30 15:34:21 -0500

    Fix paths and filename for MAFFT

--- Compiler/gcc/4.8/mafft/7.1.lua
+++ Compiler/gcc/4.8/mafft/7.1.lua
@@ -2,16 +2,18 @@ help(
 [[
 MAFFT is a program for multiple alignment program for amino acid or nucleotide sequences.
 
-Version 7.182
+Version 7.1
 ]]
 )
 
+help(help_message,"\n")
+
 whatis("Name: MAFFT")
-whatis("Version: 7.182")
+whatis("Version: 7.1")
 whatis("Category: computational biology, alignments")
 whatis("Keywords: Biology, Genomics, alignments")
 whatis("URL: http://mafft.cbrc.jp")
 whatis("Description: MAFFT - a generic tool for sequence alingments.")
 
-prepend_path("PATH",              "/util/opt/BCRF/mafft/7.182/gcc/4.8/bin")
-prepend_path("MANPATH",		  "/util/opt/BCRF/mafft/7.182/share/man/man1")
+prepend_path("PATH",              "/util/opt/BCRF/mafft/7.1/gcc/4.8/bin")
+prepend_path("MANPATH",		  "/util/opt/BCRF/mafft/7.1/gcc/4.8/share/man")
