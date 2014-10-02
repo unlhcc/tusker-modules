7593067d4994d1ec919f27f38ba9fffb5611cbe5
Adam Caprez <acaprez2@unl.edu>
2014-10-02 15:22:16 -0500

    Add module files for UDUNITS 2.1
    Module files for GCC 4.8, Intel 13, PGI 13/14, Open64 4.5.

---
+++ Compiler/open64/4.5/udunits/2.1.lua
@@ -0,0 +1,22 @@
+local help_message = [[
+udunits 2.1
+
+This module loads the udunits2 package, built with the Open64
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: udunits2")
+whatis("Version: 2.1")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.unidata.ucar.edu/software/udunits")
+
+prepend_path("PATH",		    "/util/opt/udunits/2.1/open64/4.5/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/udunits/2.1/open64/4.5/lib")
+prepend_path("MANPATH",             "/util/opt/udunits/2.1/open64/4.5/share")
+prepend_path("INCLUDE",             "/util/opt/udunits/2.1/open64/4.5/include")
+
+family("udunits2")
