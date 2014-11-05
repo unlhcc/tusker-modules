686695a46c0684e12a661b4106ea04302cf9a3be
Adam Caprez <acaprez2@unl.edu>
2014-11-05 16:23:30 -0600

    Add module for GDAL 1.11

---
+++ Compiler/gcc/4.7/gdal/1.11.lua
@@ -0,0 +1,19 @@
+local help_message = [[
+GDAL 1.11
+
+This module loads the GDAL package, built with the GCC
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: gdal")
+whatis("Version: 1.11")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.gdal.org")
+
+prepend_path("INCLUDE"		,"/util/opt/gdal/1.11/gcc/4.7/include")
+prepend_path("LD_LIBRARY_PATH"	,"/util/opt/gdal/1.11/gcc/4.7/lib")
+prepend_path("PATH"		,"/util/opt/gdal/1.11/gcc/4.7/bin")
