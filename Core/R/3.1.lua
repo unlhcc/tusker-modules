9f00ddbee2adcc867b9f9e74a2690823ad3f7160
Adam Caprez <acaprez2@unl.edu>
2014-11-10 13:54:49 -0600

    Add LD_LIBRARY_PATH for libR.so

--- Core/R/3.1.lua
+++ Core/R/3.1.lua
@@ -16,6 +16,7 @@ whatis("URL: http://www.r-project.org/")
 
 prepend_path("PATH",                "/util/opt/R/3.1/gcc/4.4/bin")
 prepend_path("LD_LIBRARY_PATH",     "/util/opt/R/3.1/gcc/4.4/lib64")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/R/3.1/gcc/4.4/lib64/R/lib")
 prepend_path("LD_LIBRARY_PATH",     "/util/opt/acml/4.4.0/gfortran64_mp/lib")
 prepend_path("MANPATH",             "/util/opt/R/3.1/gcc/4.4/share/man/man1")
 
