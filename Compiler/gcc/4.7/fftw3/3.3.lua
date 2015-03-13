b3ec66f8ebf203d8e9d07c864dac52d776eae877
Adam Caprez <acaprez2@unl.edu>
2015-03-13 11:11:44 -0500

    Changed setting INCLUDE to FFTW3_INCLUDE
    Doing 'prepend_path' on INCLUDE won't work, as the '-I' option
    for include paths doesn't support multiple colon separated paths.
    Instead, set an env variable specific to the package.

--- Compiler/gcc/4.7/fftw3/3.3.lua
+++ Compiler/gcc/4.7/fftw3/3.3.lua
@@ -17,6 +17,6 @@ whatis("URL: http://www.fftw.org/index.html")
 prepend_path("PATH",                "/util/opt/fftw3/3.3/gcc/4.7/bin")
 prepend_path("LD_LIBRARY_PATH",     "/util/opt/fftw3/3.3/gcc/4.7/lib")
 prepend_path("MANPATH",             "/util/opt/fftw3/3.3/gcc/4.7/share/man")
-prepend_path("INCLUDE",             "/util/opt/fftw3/3.3/gcc/4.7/include")
+setenv("FFTW3_INCLUDE",       	    "/util/opt/fftw3/3.3/gcc/4.7/include")
 
 family("fftw3")
