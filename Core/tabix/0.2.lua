1b8e93e5583a9ddfae3a010658dc695eb23ad53f
Adam Caprez <acaprez2@unl.edu>
2015-03-20 16:37:25 -0500

    Fix typo

--- Core/tabix/0.2.lua
+++ Core/tabix/0.2.lua
@@ -1,7 +1,7 @@
 help (
 [[
 The tabix module file defines the following environment variables:
-PATH for the location of the muscle distribution binary. 
+PATH for the location of the tabix distribution binary. 
 
 Version 0.2
 
@@ -12,7 +12,7 @@ whatis("Version: 0.2")
 whatis("Category: computational biology, genomics")
 whatis("Keywords: Biology, Genomics, Alignment, Sequencing")
 whatis("URL: http://samtools.sourceforge.net/tabix.shtml")
-whatis("Description: Tabix indexes a TAB-delimited genome position file in.tab.bgz and creates an index file")
+whatis("Description: Tabix indexes a TAB-delimited genome position file in.tab.bgz and creates an index file.")
 
 prepend_path("PATH"       ,"/util/opt/tabix/0.2/gcc/4.4/bin")
 
