719e2fd58b93ae052b837271e1ac89074dd5f55d
Adam Caprez <acaprez2@unl.edu>
2015-04-08 14:09:26 -0500

    Add module file for GeneWise 2.4

---
+++ Core/genewise/2.4.lua
@@ -0,0 +1,21 @@
+help (
+[[
+The genewise module file defines the following environment variables:
+PATH for the location of the genewise executables. 
+WISECONFIGDIR for the location of the config directory.
+
+Documentation can be found online at http://www.ebi.ac.uk/Tools/psa/genewise/help
+
+Version 2.4
+]])
+
+whatis("Name: Genewise")
+whatis("Version: 2.4")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, Utility, Sequencing")
+whatis("URL: http://www.ebi.ac.uk/Tools/psa/genewise/help")
+whatis("Description: The Wise2 form compares a protein sequence to a genomic DNA sequence, allowing for introns and frameshifting errors.")
+
+prepend_path("PATH"       ,"/util/opt/genewise/2.4/gcc/4.4/src/bin")
+prepend_path("LD_LIBRARY_PATH",	"/util/opt/genewise/2.4/gcc/4.4/lib64")
+setenv("WISECONFIGDIR"	  ,"/util/opt/genewise/2.4/gcc/4.4/wisecfg")
