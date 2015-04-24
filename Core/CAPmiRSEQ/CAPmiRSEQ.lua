836b48aa92afa1e80bfdde4e3116e316de7d1176
Jingchao Zhang <jzhang30@unl.edu>
2015-04-24 12:32:41 -0500

    installed CAPmiRSEQ pipeline on Tusker

---
+++ Core/CAPmiRSEQ/CAPmiRSEQ.lua
@@ -0,0 +1,21 @@
+local help_message = [[
+CAP-miRNASeq Pipeline
+A comprehensive analysis pipeline for deep microRNA sequencing
+
+This module loads the CAP-miRNASeq environment, built with the GCC
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: CAPmiRSEQ")
+whatis("Version: 1.0")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://bioinformaticstools.mayo.edu/research/cap-mirseq/")
+
+load("R/3.0","python/2.7","HTSeq/0.6.1p1")
+setenv("R_LIBS", "/util/src/CAPmiRSEQ/R_dependencies")
+
+family("CAPmiRSEQ")
