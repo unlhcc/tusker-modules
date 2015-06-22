0c9b8a2c9e083949820456c6698d188cb0aded2d
Adam Caprez <acaprez2@unl.edu>
2015-06-22 15:49:49 -0500

    Change over to Anaconda Python

--- Core/HTSeq/0.6.1p1.lua
+++ Core/HTSeq/0.6.1p1.lua
@@ -3,7 +3,6 @@ help (
 This module loads htseq, which depends on python and numpy.
 To call this function, use "import HTSeq" within python.
 Documentation for htseq is available online at the publisher website: http://www-huber.embl.de/users/anders/HTSeq/doc/overview.html
-PYTHONPATH has been prepended to include the HTSeq library.
 Version 0.6.1p1
 ]])
 
@@ -14,8 +13,7 @@ whatis("Keywords: Biology, Genomics, High-throughput Sequencing")
 whatis("Description: HTSeq - Analysing high-throughput sequencing data with Python")
 whatis("URL: https://pypi.python.org/pypi/HTSeq")
 
-prepend_path("PATH"		,"/util/opt/HTSeq/0.6.1p1/gcc/4.4/bin")
-prepend_path("PYTHONPATH"       ,"/util/opt/HTSeq/0.6.1p1/gcc/4.4/lib/python2.7/site-packages")
-
-prereq("python/2.7")
-
+prepend_path{"PATH"             ,"/util/opt/anaconda/2.0/envs/htseq-0.6.1p1/bin",priority=100}
+pushenv("CONDA_DEFAULT_ENV"     ,"htseq-0.6.1p1")
+--prereq("python/2.7")
+family("anaconda")
--- Core/python/2.7.lua
+++ Core/python/2.7.lua
@@ -1,14 +1,14 @@
 local help_message = [[
 Python 2.7
 
-This module loads the Python environment, built with the GCC
-compilers.
+This module loads the Anaconda Python environment.
+See https://store.continuum.io/cshop/anaconda.
 
 Commonly used installed packages include:
 Numpy
 Scipy
 Matplotlib
-BioPython
+Biopython
 
 ]]
 
@@ -20,11 +20,12 @@ whatis("Category: system, development")
 whatis("Keywords: System, Development, Python")
 whatis("URL: http://www.python.org/")
 
-prepend_path("PATH",                "/util/opt/python/2.7/gcc/4.4/bin")
-prepend_path("LD_LIBRARY_PATH",     "/util/opt/python/2.7/gcc/4.4/lib")
-prepend_path("MANPATH",             "/util/opt/python/2.7/gcc/4.4/share/man")
+prepend_path("PATH",                "/util/opt/anaconda/2.0/bin")
+prepend_path("MANPATH",             "/util/opt/anaconda/2.0/share/man")
 
-prepend_path("PATH",		"/util/opt/setuptools/5.1/gcc/4.4/bin")
-prepend_path("PYTHONPATH",	"/util/opt/setuptools/5.1/gcc/4.4/lib/python2.7/site-packages")
+append_path("CONDA_ENVS_PATH", "~/.conda/envs")
+local group_conda_env = os.getenv("CONDA_GROUP_ENV")
+append_path("CONDA_ENVS_PATH", group_conda_env .. "/2")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/2.0.1/envs")
 
 family("python")
