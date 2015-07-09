0f6cf6f69f2ba911d9ae02e3a3cc499e7ecc37ae
Jingchao Zhang <jzhang30@unl.edu>
2015-07-09 09:58:48 -0500

    Updated LAMMPS to stable version 15May15

---
+++ MPI/gcc/4.9/openmpi/1.8/lammps/15May15.lua
@@ -0,0 +1,19 @@
+local help_message = [[
+LAMMPS Stable Version 15/05/2015
+
+This module loads the lammps environment, built with the GCC
+compiler/4.9 and openmpi/1.8.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: LAMMPS")
+whatis("Version: 15May15")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://lammps.sandia.gov/")
+
+prepend_path("PATH",                "/util/opt/lammps/15May15/gcc/4.9/bin")
+
+family("lammps")
