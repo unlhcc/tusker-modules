c74e963a78645ce17a172c662fdc2221f6c85a14
Jingchao Zhang <jzhang30@unl.edu>
2015-07-24 11:05:07 -0500

    HDf5 with OpenMPI-1.8 on Tusker

---
+++ MPI/pgi/11/openmpi/1.8/hdf5/1.8.13.lua
@@ -0,0 +1,21 @@
+local help_message = [[
+HDF5 1.8.13
+
+This module loads the HDF5, built with the PGI
+compilers and OpenMPI.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: HDF5")
+whatis("Version: 1.8.13")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: https://www.hdfgroup.org/HDF5/")
+
+prepend_path("PATH",                "/util/opt/hdf5/1.8.13/openmpi/1.8/pgi/11/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/hdf5/1.8.13/openmpi/1.8/pgi/11/lib")
+prepend_path("INCLUDE",             "/util/opt/hdf5/1.8.13/openmpi/1.8/pgi/11/include")
+
+family("hdf5")
