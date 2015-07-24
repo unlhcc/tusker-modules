02c76d8bde631a0eee227cff8a5ba1c8a3638422
Jun Wang <jwang@login.tusker.hcc.unl.edu>
2015-07-24 13:55:36 -0500

    renamed:    MPI/intel/13/nwchem/6.3.lua -> MPI/intel/13/openmpi/1.8/nwchem/6.5.lua 	modified:   tusker_modules.xml

--- MPI/intel/13/openmpi/1.8/nwchem/6.5.lua
+++ MPI/intel/13/openmpi/1.8/nwchem/6.5.lua
@@ -1,21 +1,19 @@
 local help_message = [[
-NWChem 6.3
+NWChem 6.5
 
-This module loads the nwchem environment, built with the Intel
+This module loads the nwchem environment, built with the intel 13
 compilers.
 
 ]]
 
 help(help_message,"\n")
 
-whatis("Name: NWCHEM")
-whatis("Version: 6.3")
+whatis("Name: NWChem")
+whatis("Version: 6.5")
 whatis("Category: library, runtime support")
 whatis("Keywords: Applications, Chemistry")
 whatis("URL: http://www.nwchem-sw.org/index.php/Main_Page")
 
-load("intel-mkl/13")
-
-prepend_path("PATH","/util/opt/nwchem/6.3/bin/LINUX64")
+prepend_path("PATH","/util/opt/nwchem/6.5/bin/LINUX64")
 
 family("nwchem")
