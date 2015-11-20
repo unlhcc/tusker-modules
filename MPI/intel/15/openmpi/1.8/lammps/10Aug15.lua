de04df5e2363487784c5483a0b5b987f8d1396fe
Jingchao Zhang <zhang@unl.edu>
2015-11-20 14:37:36 -0600

    LAMMPS 10/Aug/2015 with compiler/intel/15 and openmpi/1.8

---
+++ MPI/intel/15/openmpi/1.8/lammps/10Aug15.lua
@@ -0,0 +1,18 @@
+local help_message = [[
+LAMMPS Stable Version 10/Aug/2015
+
+This module loads the lammps environment, built with the Intel Compiler 15 and openmpi/1.8.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: LAMMPS")
+whatis("Version: 10Aug15")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://lammps.sandia.gov/")
+
+prepend_path("PATH",                "/util/opt/lammps/10Aug15/openmpi/1.8/intel/15/bin")
+
+family("lammps")
