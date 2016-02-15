771856b52105bfed46588aa932cc35c3d2a7cc4a
Jingchao Zhang <zhang@unl.edu>
2016-02-15 12:22:30 -0600

    parallel hdf5 on Tusker with pgi/12 and openmpi/1.8

---
+++ MPI/pgi/12/openmpi/1.8/phdf/1.8.lua
@@ -0,0 +1,21 @@
+local help_message = [[
+Parallel hdf5 1.8.12
+
+This module loads the hdf5, built with the PGI, OpenMPI
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: hdf5")
+whatis("Version: 1.8.12")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.hdfgroup.org/HDF5/release/obtain5.html#obtain")
+
+prepend_path("PATH",                "/util/opt/hdf5/1.8.12/openmpi/1.8/pgi/12/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/hdf5/1.8.12/openmpi/1.8/pgi/12/lib")
+prepend_path("INCLUDE",             "/util/opt/hdf5/1.8.12/openmpi/1.8/pgi/12/include")
+
+family("phdf5")
