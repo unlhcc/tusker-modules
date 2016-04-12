67e17a71a8bae7a7d344c959489a5ae90293fd4d
Adam Caprez <acaprez2@unl.edu>
2016-04-12 10:57:27 -0500

    Add module file for jmosaics 1.10.0

---
+++ Core/jmosaics/1.10.lua
@@ -0,0 +1,15 @@
+help (
+[[
+This module loads jmosaics
+Version 1.10
+]])
+
+whatis("Name: jmosaics")
+whatis("Version: 1.10")
+whatis("Category: computational biology, sequencing")
+whatis("Keywords: Biology, Genomics, Sequencing")
+whatis("Description: Joint analysis of multiple ChIP-Seq data sets.")
+whatis("URL: https://bioconductor.org/packages/jmosaics")
+
+prepend_path{"PATH"		,"/util/opt/anaconda/2.0/envs/jmosaics-1.10.0/bin",priority=100}
+pushenv("CONDA_DEFAULT_ENV"	,"jmosaics-1.10.0")
