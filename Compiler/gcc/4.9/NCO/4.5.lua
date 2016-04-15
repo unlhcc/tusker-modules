6f85cc36bf4dc23fd78fc725ea471e012597fa07
Jingchao Zhang <zhang@unl.edu>
2016-04-15 14:51:49 -0500

    NCO version 4.5.5 with all compilers

---
+++ Compiler/gcc/4.9/NCO/4.5.lua
@@ -0,0 +1,25 @@
+local help_message = [[
+NCO 4.5
+
+This module loads the NetCDF Operators package, built with
+the GCC compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: NCO")
+whatis("Version: 4.5")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://nco.sourceforge.net")
+
+prepend_path("PATH",		    "/util/opt/NCO/4.5/gcc/4.9/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/NCO/4.5/gcc/4.9/lib")
+prepend_path("MANPATH",             "/util/opt/NCO/4.5/gcc/4.9/share/man")
+prepend_path("INCLUDE",             "/util/opt/NCO/4.5/gcc/4.9/include")
+
+family("NCO")
+
+prepend_path("LIBRARY_PATH",     "/util/opt/NCO/4.5/gcc/4.9/lib")
+prepend_path("CPATH",             "/util/opt/NCO/4.5/gcc/4.9/include")
