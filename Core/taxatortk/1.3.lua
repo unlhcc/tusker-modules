64f28f0261c881198ea74f2d9a79dd2680673a83
Jingchao Zhang <zhang@unl.edu>
2016-07-26 14:58:31 -0500

    taxatortk on Tusker

---
+++ Core/taxatortk/1.3.lua
@@ -0,0 +1,15 @@
+help (
+[[
+taxator-tk is an algorithm and the accompanying software, which performs taxonomic sequence assignment by fast approximate determination of evolutionary neighbors from sequence similarities.
+Please see Dröge et a. 2014 for details. The source code and issue tracker can be found at GitHub We provide a set of example refpacks for use with taxator-tk.
+]])
+
+whatis("Name: taxator-tk")
+whatis("Version: 1.3.3")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, Sequencing, Assembly")
+whatis("Description: taxator-tk")
+whatis("URL: http://research.bifo.helmholtz-hzi.de/software/")
+
+prepend_path("PATH"       ,"/util/opt/taxatortk/1.3/gcc/4.4")
+prepend_path("PATH"       ,"/util/opt/taxatortk/1.3/gcc/4.4/bin")
