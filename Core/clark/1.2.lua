9e5b42407facf2c26a02f93581fc043b19a352d8
Jingchao Zhang <zhang@unl.edu>
2016-07-26 10:47:22 -0500

    clark on Tusker

---
+++ Core/clark/1.2.lua
@@ -0,0 +1,14 @@
+help (
+[[
+CLARK is a method based on a supervised sequence classification using discriminative k-mers.
+
+Version 1.2.3
+]])
+
+whatis("Name: CLARK")
+whatis("Version: 1.2.3")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, Sequencing, Assembly")
+whatis("URL: http://clark.cs.ucr.edu/")
+
+prepend_path("PATH"       ,"/util/opt/clark/1.2/gcc/4.4/bin")
