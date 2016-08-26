ea6e0d99a69ab4168fea6ba98b8199dc7933c30e
Adam Caprez <acaprez2@unl.edu>
2016-08-26 11:36:30 -0500

    Add LD_LIBRARY_PATH for needed boost library.

--- Core/tophat/2.0.9.lua
+++ Core/tophat/2.0.9.lua
@@ -15,4 +15,4 @@ whatis("URL: http://tophat.cbcb.umd.edu/")
 whatis("Description: TopHat2 is a fast splice junction mapper for RNA-Seq reads. It aligns RNA-Seq reads to mammalian-sized genomes using the ultra high-throughput short read aligner Bowtie, and then analyzes the mapping results to identify splice junctions between exons.")
 
 prepend_path("PATH",              "/util/opt/tophat/2.0.9/gcc/4.4/bin")
-
+prepend_path("LD_LIBRARY_PATH",	  "/util/opt/boost/1.51.0/gcc/4.4.5/lib")
