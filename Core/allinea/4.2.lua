6b85c77e1870e9f27187c589f76025884e192d04
John Thiltges <jthiltges2@unl.edu>
2017-01-24 15:30:54 -0600

    Fix expansion of $WORK when setting Allinea ALLINEA_TOOLS_CONFIG_DIR

--- Core/allinea/4.2.lua
+++ Core/allinea/4.2.lua
@@ -16,6 +16,6 @@ whatis("URL: http://www.allinea.com")
 
 prepend_path("PATH",                "/util/opt/allinea/4.2/bin")
 prepend_path("LD_LIBRARY_PATH",     "/util/opt/allinea/4.2/lib")
-setenv("ALLINEA_TOOLS_CONFIG_DIR","$WORK/.allinea")
+setenv("ALLINEA_TOOLS_CONFIG_DIR",pathJoin(os.getenv("WORK"),".allinea"))
 
 prepend_path("LIBRARY_PATH",     "/util/opt/allinea/4.2/lib")
