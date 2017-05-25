87f7916e78a62c6ffbcf3731ed3040cce701f177
Adam Caprez <acaprez2@unl.edu>
2017-05-25 16:44:31 -0500

    Add module files for Theano 0.9.0.

---
+++ Core/theano/py27/0.9.lua
@@ -0,0 +1,17 @@
+help (
+[[
+This module loads Theano
+Version 0.9.0
+]])
+
+whatis("Name: Theano")
+whatis("Version: 0.9.0")
+whatis("Category: mathematics, machine learning")
+whatis("Keywords: Mathematics, Machine Learning")
+whatis("Description: Theano is a Python library that allows you to define, optimize, and evaluate mathematical expressions involving multi-dimensional arrays efficiently.")
+whatis("URL: http://deeplearning.net/software/theano/index.html")
+
+prepend_path{"PATH"		,"/util/opt/anaconda/4.3/envs/theano-0.9.0-py27/bin",priority=100}
+pushenv("CONDA_DEFAULT_ENV"	,"theano-0.9.0-py27")
+
+family("anaconda")
