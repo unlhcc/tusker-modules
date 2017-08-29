f09e2a16194100f44d58275c92449be2458f8ccd
Natasha Pavlovikj <npavlovikj2@unl.edu>
2017-08-29 12:37:21 -0500

    Add module file for quast/4.5, update module file for fastqc/0.11.5 and update .xml file

---
+++ update_modules.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/util/admin/bin/create_module_xmlfile.sh /home/deogun/npavlovikj/HCC_MODULES/tusker-modules/Core > /home/deogun/npavlovikj/HCC_MODULES/tusker-modules/tusker_modules.xml
