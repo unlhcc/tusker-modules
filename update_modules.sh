c43114e3474091860b382d4a25dde0c16c7cc58d
Natasha Pavlovikj <npavlovikj2@unl.edu>
2017-08-29 12:56:55 -0500

    Delete unwanted commits

--- update_modules.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/util/admin/bin/create_module_xmlfile.sh /home/deogun/npavlovikj/HCC_MODULES/tusker-modules/Core > /home/deogun/npavlovikj/HCC_MODULES/tusker-modules/tusker_modules.xml
