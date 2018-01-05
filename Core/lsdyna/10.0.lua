158ea1a2aa1bec5a1e0891ee64d2681e61e4571d
Tom Harvill <tharvill@login.tusker.hcc.unl.edu>
2018-01-05 14:31:55 -0600

    New LS-DYNA version modules

---
+++ Core/lsdyna/10.0.lua
@@ -0,0 +1,20 @@
+local help_message = [[
+LS-DYNA (Finite Element Analysis Software)
+  Provided for and used by permission of Prof. John Reid
+
+# This is a special dev version of LS-DYNA ##
+
+mpp971_s    = Single precision parallel (OpenMPI)
+mpp971_d    = Double precision parallel (OpnenMPI)
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: LS-DYNA")
+whatis("Keywords: Application, Engineering")
+
+load("compiler/intel/16","openmpi/1.10")
+
+
+prepend_path("PATH","/util/opt/lsdyna/r10.0/")
